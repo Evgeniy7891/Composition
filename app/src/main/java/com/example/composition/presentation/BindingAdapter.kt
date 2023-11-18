f8a39187094c2e9ea2ec3cecc97582a9d6cdefef
Evgeniy Sterekhov <john2331832@gmail.com>
2023-11-18 22:40:12 +0500

    Data binding observer. 53

--- app/src/main/java/com/example/composition/presentation/BindingAdapter.kt
+++ app/src/main/java/com/example/composition/presentation/BindingAdapter.kt
@@ -1,10 +1,18 @@
 package com.example.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.composition.R
 import com.example.composition.domain.entity.GameResult
+
+interface OnOptionsClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -55,5 +63,34 @@ private fun getSmileResId(winner: Boolean): Int {
         R.drawable.ic_smile
     } else {
         R.drawable.ic_sad
+    }
+}
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+private fun getColorByState(context: Context, getState: Boolean): Int {
+    val colorResId = if (getState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionsClickListener")
+fun bindOnOptionsClickListener(textView: TextView, clickListener: OnOptionsClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
     }
 }
